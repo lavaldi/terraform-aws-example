03948063a265890669ade7b8e39325d17b8caa65
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-07-03 16:08:34 -0500

    refactor to new way of calling variables

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,8 +1,8 @@
 # Create a EC2
 resource "aws_instance" "ec2_instance_example" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "${var.instance_type}"
-  subnet_id     = "${data.aws_subnet.example.id}"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = data.aws_subnet.example.id
   tags = {
     Name = "lavaldi"
   }
